3dbdb57cf2c78a11a07ad25f2b8bb326c746f6a6
kaweng pam <kawengpam56@gmail.com>
2024-10-10 14:52:02 +0100

    added forecast style and injected the forcast through Javascript

--- src/index.js
+++ src/index.js
@@ -55,4 +55,28 @@ function handleSearch(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+          <div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">🌤️</div>
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temp">
+                <strong>15°</strong>
+              </div>
+              <div class="weather-forecast-temp">9°</div>
+          </div>
+           </div>`;
+  });
+
+  let forecast = document.querySelector("#forecast");
+  forecast.innerHTML = forecastHtml;
+}
 searchCity("abuja");
+displayForecast();
